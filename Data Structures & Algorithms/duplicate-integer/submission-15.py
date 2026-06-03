e74493caefd80280ead162d17d538f187f5651ca
rogelio-9 <146398663+rogelio-9@users.noreply.github.com>
2026-06-03 16:15:34 -0600

    Remove: duplicate-integer - submission-15

--- Data Structures & Algorithms/duplicate-integer/submission-15.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        if len(nums) == len(set(nums)):
-            return True
-        return False
-
-        
-            
-        
